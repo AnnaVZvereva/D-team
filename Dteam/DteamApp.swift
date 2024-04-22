f3224c899c5b8b92c7a5f00fe88f48e09ede9600
AnnaVZvereva <148349001+AnnaVZvereva@users.noreply.github.com>
2024-04-22 07:22:52 -0400

    Initial Commit

---
+++ Dteam/DteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  DteamApp.swift
+//  Dteam
+//
+//  Created by Anna Zvereva on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct DteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
